4249cdb45f85b9c9becf40a1ea41e561fd04a79b
Mohsin Raza <frddbrn@gmail.com>
2024-02-10 14:43:36 +0500

    update: remove links

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
       <div className="flex flex-col justify-center items-center">
         <p class="pb-2">Contact</p>
         <div className="flex gap-3 justify-center">
-          <a href="https://twitter.com/mohsinabro666">
-            <BsTwitter size={24} />
-          </a>
-          <a href="https://www.instagram.com/unholyfiend">
-            <BsInstagram size={24} />
-          </a>
+          // <a href="https://twitter.com/mohsinabro666">
+          //   <BsTwitter size={24} />
+          // </a>
+          // <a href="https://www.instagram.com/unholyfiend">
+          //   <BsInstagram size={24} />
+          // </a>
           <a href="https://www.linkedin.com/in/mohsin-raza-a93798184/">
             <BsLinkedin size={24} />
           </a>
